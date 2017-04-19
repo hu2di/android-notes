52fd511359c9c07e6cbe5f4cc86cad94b5439183
HuyHung Dinh <hebitaxy@gmail.com>
2017-04-19 09:02:04 +0700

    Update FileUtil.java

--- FileUtil.java
+++ FileUtil.java
@@ -121,4 +121,19 @@ public class FileUtils {
                     }
                 });
     }
+    
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = {MediaStore.Images.Media.DATA};
+            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
 }
