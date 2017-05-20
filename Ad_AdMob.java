33ca4914e4336d1edd5c4590777088cfd59d4319
HuyHung Dinh <hebitaxy@gmail.com>
2017-05-20 10:01:17 +0700

    Update Ad_AdMob.java

--- Ad_AdMob.java
+++ Ad_AdMob.java
@@ -1,9 +1,10 @@
 compile 'com.google.android.gms:play-services-ads:10.2.0'
 
-<string name="id_native_result_unit">ca-app-pub-9414566302380491/2411858161</string>
+<string name="native_ad_unit_id">ca-app-pub-3940256099942544/2177258514</string>
 <string name="banner_ad_unit_id">ca-app-pub-3940256099942544/6300978111</string>
 <string name="interstitial_ad_unit_id">ca-app-pub-3940256099942544/1033173712</string>
 
+//BANNER
 xmlns:ads="http://schemas.android.com/apk/res-auto"
 
 <com.google.android.gms.ads.AdView
@@ -36,6 +37,7 @@ private void initBanner() {
     mAdView.loadAd(adRequest);
 }
 
+//INTERSTITIAL
 private InterstitialAd mInterstitialAd;
 
 private void initInterstitialAd() {
@@ -65,3 +67,35 @@ private void showInterstitial() {
         mInterstitialAd.show();
     }
 }
+
+//NATIVE
+<com.google.android.gms.ads.NativeExpressAdView
+    android:id="@+id/adView"
+    android:layout_width="wrap_content"
+    android:layout_height="wrap_content"
+    android:layout_centerHorizontal="true"
+    android:layout_alignParentBottom="true"
+    ads:adSize="320x300"
+    ads:adUnitId="@string/native_ad_unit_id">
+</com.google.android.gms.ads.NativeExpressAdView>
+
+private void initNativeAds() {
+    final NativeExpressAdView nativeAds = (NativeExpressAdView) findViewById(R.id.adViewExpress);
+    nativeAds.setAdListener(new AdListener() {
+        @Override
+        public void onAdLoaded() {
+            super.onAdLoaded();
+            nativeAds.setVisibility(View.VISIBLE);
+        }
+	@Override
+        public void onAdFailedToLoad(int i) {
+            super.onAdFailedToLoad(i);
+            nativeAds.setVisibility(View.GONE);
+        }
+    });
+    AdRequest adRequest = new AdRequest.Builder()
+          //.addTestDevice("8B6D4ADFA9FB66F0468D16879194A3DE")
+            .build();
+nativeAds.loadAd(adRequest);
+}
+
